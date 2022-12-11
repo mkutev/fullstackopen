4f692f11a7f93980a7fe91264dc44d6f6549f73e
Momchil Kutev <momchil.kutev@gmail.com>
2022-12-11 21:04:28 +0000

    Added Exercise 1 for Part 1

--- part1/src/App.js
+++ part1/src/App.js
@@ -8,21 +8,32 @@
 
 // export default App;
 
-const Hello = () => {
+const Time = () => {
+	const now = new Date();
+
 	return (
 		<div>
-			<p>Hello world</p>
+			<p>The time is now {now.toString()}</p>
 		</div>
 	);
 };
-const App = () => {
+
+const Greating = (props) => {
 	return (
-		<div>
-			<h1>Greetings</h1>
-			<Hello />
-      <Hello />
-      <Hello />
-		</div>
+		<h1>Welcome {props.name}, you are {props.age} years old.</h1>
+	);
+};
+
+const App = () => {
+	const name = 'Guy'
+	const age = 3
+
+	return (
+		<>
+			<Greating name='Buddy' age={age} />
+			<Greating name={name} age={age} />
+			<Time /> 
+		</>
 	);
 };
 
