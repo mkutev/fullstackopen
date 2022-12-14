ff59d2e7b9f66fd2e8f6dee4bb57b275ece32c01
Momchil Kutev <momchil.kutev@gmail.com>
2022-12-14 06:21:07 +0000

    Fixed courseinfo exercises

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,33 @@
-import { useState } from "react";
-
 const App = () => {
-	const [left, setLeft] = useState(0)
-	const [right, setRight] = useState(0)
-	const [allClicks, setAll] = useState([])
-	
-	const handleLeftClick = () => { 
-		setAll(allClicks.concat('L')) 
-		setLeft(left + 1)
+
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10
+			},
+			{
+				name: 'Usings props to pass data',
+				exercises: 7
+			},
+			{
+				name: 'State of a component',
+				exercises: 14
+			}
+		]
 	}
-	const handleRightClick = () => { 
-		setAll(allClicks.concat('R')) 
-		setRight(right + 1)
-	}
-	
+
+
 	return (
-	  <div>
-		 {left}
-		 <button onClick={handleLeftClick}>left</button>
-		 <button onClick={handleRightClick}>right</button>
-		 {right}
-		 <p>{allClicks.join(' ')}</p>    </div>
-	)
- }
+		<div>
+			<h1>{course.name}</h1>
+			<p>{course.parts[0].name} {course.parts[0].exercises}</p>
+			<p>{course.parts[1].name} {course.parts[1].exercises}</p>
+			<p>{course.parts[2].name} {course.parts[2].exercises}</p>
+			<p>Total Exercises: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+		</div>
+	);
+};
 
 export default App;
