b545b46e0769fb5792e959f949024cf614891e30
Momchil Kutev <momchil.kutev@gmail.com>
2022-12-14 07:13:02 +0000

    Exercise 1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+
+const App = () => {
+  const [ goodCount, setGoodCount] = useState(0)
+  const [ mehCount, setMehAmount] = useState(0)
+  const [ badCount, setBadCount] = useState(0)
+  const [ total, setTotal ] = useState(0)
+  const good = () => {
+    setGoodCount(goodCount + 1)
+    setTotal(total+1)
+  } 
+  const meh = () => {
+    setMehAmount(mehCount + 1)
+    setTotal(total+1)
+  }
+  const bad = () => {
+    setBadCount(badCount + 1)
+    setTotal(total+1)
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Give Feedback</h1>
+      <Button onClick={ good } name='Good' />
+      <Button onClick={ meh } name='Meh' />  
+      <Button onClick={ bad } name='Bad' />  
+      <h2>Statistics</h2>
+      <p>Good: {goodCount}</p>
+      <p>Meh: {mehCount}</p>
+      <p>Bad: {badCount}</p>
+      <br />
+      <p>Total: {total}</p>
+      <p>Average: {(goodCount+mehCount*0+badCount*-1)/total}</p>
+      <p>Positive: {goodCount/total} %</p>
     </div>
-  );
+  )
 }
+
+const Button = ({onClick, name}) => <button onClick={onClick}>{name}</button>
 
 export default App;
